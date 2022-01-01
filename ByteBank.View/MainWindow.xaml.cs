c631cd48fb05006445a79a42469b65a3d0d6a702
Alan <alanbritodossantos2015@gmail.com>
2022-01-01 03:03:09 -0300

    Usando AsyncAwait

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -36,11 +36,12 @@ namespace ByteBank.View
 
 
         //é executado quando o usuario clica no botão
-        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
+
+        private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
             //é executado na TaskSheduler principal
             //dependendo de onde ele é executado terá um resultado diferente
-            var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();//permite se executado na interface grafica a task 
+            //var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();//permite se executado na interface grafica a task 
             BtnProcessar.IsEnabled = false;
 
             //todas as contas dos clientes é armazenado na variavel "contas"
@@ -54,49 +55,30 @@ namespace ByteBank.View
             //contador.. armazena o inicio da operação
             var inicio = DateTime.Now;
 
+            //await significa aguardar uma tarefa
+            var resultado = await ConsolidarContas(contas);
 
-
-
-            //espera terminar outras tarefas(é uma tarefa que serve para esperar outras tarefas)
-            ConsolidarContas(contas)
-                .ContinueWith(task => //encadeando uma task
-                {
-                    var fim = DateTime.Now;
-                    var resultado = task.Result;
-                    AtualizarView(resultado, fim - inicio);
-                }, taskSchedulerUI)// esse taskSchedulerUI vai se executado na TaskScheduler(na interface grafica) que estiver rodando no momento 
-                .ContinueWith(task => //encadeando uma task
-                {
-                    BtnProcessar.IsEnabled = true;
-                }, taskSchedulerUI);
+            var fim = DateTime.Now;
+            AtualizarView(resultado, fim - inicio);
+            BtnProcessar.IsEnabled = true;           
 
         }
 
         //retorna uma lista de string
-        private Task<List<string>> ConsolidarContas(IEnumerable<ContaCliente> contas)
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas)
         {
             var resultado = new List<string>();
             //               vai mapear as contas
-            var tasks = contas.Select(conta =>
-            {
-                return Task.Factory.StartNew(() =>
-                {
-                    var contaResultado = r_Servico.ConsolidarMovimentacao(conta);
-                });
-            });
+            var tasks = contas.Select(conta => Task.Factory.StartNew(() => r_Servico.ConsolidarMovimentacao(conta)));
 
-            ///  temos agora uma tarefa que retorna uma lista de string
-            return Task.WhenAll(tasks).ContinueWith(t => //aguarda o termino das task em execução, assim que termina deixa seguir
-            {
-                return resultado;
-            });
+            return await Task.WhenAll(tasks);
         }
 
         //Mostra o resumo da operação
-        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
+        private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
         {
             var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
-            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
+            var mensagem = $"Processamento de {result.Count()} clientes em {tempoDecorrido}";
 
             //atualiza os dados
             LstResultados.ItemsSource = result;
